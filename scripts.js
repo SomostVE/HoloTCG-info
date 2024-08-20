6eb9acde6a2f0d8ba744c3ee8139a6141dd7291c
SomostVE <66364264+SomostVE@users.noreply.github.com>
2024-08-20 13:06:43 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,9 +60,11 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.menu-toggle');
   const sidebar = document.querySelector('.sidebar');
+  const mainContent = document.querySelector('.main-content');
 
   menuToggle.addEventListener('click', function() {
     sidebar.classList.toggle('show');
+    mainContent.classList.toggle('expanded');
   });
 });
 
